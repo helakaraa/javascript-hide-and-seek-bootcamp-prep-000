7d3613147a69cd6231cb361dbf9757e4cd72013f
hela karaa <hela.karaa@ensi-uma.tn>
2017-07-22 14:29:35 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector)
+{
+return document.querySelector(selector);
+
+}
+function nestedTarget()
+{
+  return document.querySelector('#nested .target');
+}
+function deepestChild()
+{
+ return  document.querySelector('#grand-node div div div div');
+}
+function increaseRankBy(n)
+{
+  const lis = document.querySelectorAll('.ranked-list');
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML= (i + n).toString()
+
+}
+}
